dc2bef511d41d696f310d22158f1ff6bccef7eaa
Cameron Ross <elpenguino@gmail.com>
2012-06-03 01:18:09 -0300

    Improved unit testing

--- unittest.d
+++ unittest.d
@@ -1,56 +1,87 @@
 import std.stdio;
 import std.stream;
+import std.conv;
 
 import comp;
 import decomp;
 
 unittest {
-	void test(lazy bool expr, string label, bool expectException = false) {
-		write("[DECOMP] ");
-		try
-			write(expr ? "PASSED" : "FAILED");
-		catch (Throwable e)
-			write(expectException ? "PASSED" : "FAILED: " ~ e.msg);
-		writeln(" " ~ label);
+	int passes = 0;
+	int tests = 0;
+	bool test(lazy bool expr, string label, bool expectException = false) {
+		char[] output = "[DECOMP] ".dup;
+		bool success = false;
+		tests++;
+		try {
+			if (expr) {
+				output ~= "PASSED";
+				success = true;
+			} else
+				output ~= "FAILED";
+		} catch (Throwable e) {
+			output ~= (expectException ? "PASSED" : "FAILED: [" ~ e.msg ~ " on line " ~ text(e.line) ~ " of module " ~ e.file ~ "]");
+			if (expectException)
+				success = true;
+		}
+		writeln(output ~ " " ~ label);
+		if (success)
+			passes++;
+		return success;
 	}
-	void dcomptest(ubyte[] input, ubyte[] output, string msg, bool printResult = false) {
+	void testeqar(T, V)(T[] input, V[] output, string msg, bool expectException = false) {
+		if (!test(input == output, msg, expectException))
+			writeln(input);
+	}
+	void decomptest(ubyte[] input, ubyte[] output, string msg, bool expectException = false) {
 		int finalSize;
-		if (printResult)
-			writeln(decomp.decomp(input, finalSize));
-		test(decomp.decomp(input, finalSize) == output, "Decomp: " ~ msg);
-		test(finalSize == input.length, "Decomp: " ~ msg ~ " size");
+		ubyte[] data = decomp.decomp(input, finalSize);
+		if (!test(data == output, "Decomp: " ~ msg, expectException))
+			writeln(data);
+		if (!test(finalSize == input.length, "Decomp: " ~ msg ~ " size", expectException))
+			writeln(data);
 	}
-	import std.exception;
-	test([0,1,2].stripe(10) == [0, 1, 2, 0, 1, 2, 0, 1, 2, 0], "Striping: Int");
-	test(["hi","sup"].stripe(4) == ["hi", "sup", "hi", "sup"], "Striping: String");
-	ubyte[] testv;
-	test(testv.stripe(2) == [], "Striping: Null input exception", true);
+	void comptest(ubyte[] input, string msg, int idealsize = -1, bool expectException = false) {
+		auto data = comp.comp(input);
+		if (!test(decomp.decomp(data) == input, "Comp: " ~ msg, expectException))
+			writeln(data);
+		if (idealsize >= 0)
+			if (!test(data.length == idealsize, "Comp: " ~ msg ~ " ideal size", expectException))
+				writeln(data);
+	}
 
-	test((0).increaseval(1) == [0], "Increaseval: First value unaltered");
-	test((0).increaseval(3) == [0, 1, 2], "Increaseval: algorithm");
-	test((255).increaseval(3) == [255, 0, 1], "Increaseval: Wrapping values");
-	test((0).increaseval(0) == [], "Increaseval: Void");
+	testeqar((0).increaseval(1), [0], "Increaseval: First value unaltered");
+	testeqar((0).increaseval(3), [0, 1, 2], "Increaseval: algorithm");
+	testeqar((255).increaseval(3), [255, 0, 1], "Increaseval: Wrapping values");
+	testeqar((0).increaseval(0), [], "Increaseval: Void");
 
-	test([1].reversebits == [128], "Bit reversal: algorithm");
-	test([1, 2, 4, 5].reversebits == [128, 64, 32, 160], "Bit reversal: array");
-	test("HELLO".reversebits == [18, 162, 50, 50, 242], "Bit reversal: string");
-	test([].reversebits == [], "Bit reversal: void");
+	testeqar([cast(ubyte)1].reversebits, [cast(ubyte)128], "Bit reversal: algorithm");
+	testeqar((cast(ubyte[])[1, 2, 4, 5]).reversebits, cast(ubyte[])[128, 64, 32, 160], "Bit reversal: array");
+	testeqar("HELLO".reversebits, cast(string)[18, 162, 50, 50, 242], "Bit reversal: string");
+	testeqar([].reversebits, [], "Bit reversal: void");
+
+	testeqar([cast(uint)0x80000000].reversebits, [1], "Bit reversal: int");
 	ubyte[] testArray = [0x63, 0x00, 0xFF];
-	test(decomp.decomp(testArray) == decomp.decomp(new MemoryStream(testArray)), "Decomp: Stream == array");
+	testeqar(decomp.decomp(testArray), decomp.decomp(new MemoryStream(testArray)), "Decomp: Stream == array");
 
-	testArray = [0x80, 0xFF, 0x00, 0xFF];
-	test(decomp.decomp(testArray) == [], "Decomp: Uninitialized buffer", true);
+	test(decomp.decomp([0x80, 0xFF, 0x00, 0xFF]) == [], "Decomp: Uninitialized buffer", true);
 
-	dcomptest([0x03, 1, 3, 3, 7, 0xFF], [1, 3, 3, 7], "Uncompressed data");
-	dcomptest([0x21, 1, 0xFF], [1, 1], "Byte fill");
-	dcomptest([0x41, 1, 2, 0xFF], [1, 2, 1, 2], "Word fill");
-	dcomptest([0x63, 0, 0xFF], [0, 1, 2, 3], "Decomp: Increasing value");
-	dcomptest([0x61, 1, 0x80, 0, 0, 0xFF], [1, 2, 1], "Buffer copy");
-	dcomptest([0x61, 1, 0xA0, 0, 0, 0xFF], [1, 2, 128], "Bit-reversed Buffer copy");
-	dcomptest([0x61, 1, 0xC1, 0, 1, 0xFF], [1, 2, 2, 1], "Byte-reversed buffer copy");
+	decomptest([0x03, 1, 3, 3, 7, 0xFF], [1, 3, 3, 7], "Uncompressed data");
+	decomptest([0x21, 1, 0xFF], [1, 1], "Byte fill");
+	decomptest([0x41, 1, 2, 0xFF], [1, 2, 1, 2], "Word fill");
+	decomptest([0x63, 0, 0xFF], [0, 1, 2, 3], "Decomp: Increasing value");
+	decomptest([0x61, 1, 0x80, 0, 0, 0xFF], [1, 2, 1], "Buffer copy");
+	decomptest([0x61, 1, 0xA0, 0, 0, 0xFF], [1, 2, 128], "Bit-reversed Buffer copy");
+	decomptest([0x61, 1, 0xC1, 0, 1, 0xFF], [1, 2, 2, 1], "Byte-reversed buffer copy");
 	ubyte[] extended = new ubyte[513];
 	extended[] = 1;
-	dcomptest([0xE6, 0, 1, 0xFF], extended, "Extended byte fill");
+	decomptest([0xE6, 0, 1, 0xFF], extended, "Extended byte fill");
 
-	test(decomp.decomp(comp.comp([1,3,3,7])) == [1,3,3,7], "Compression");
+	comptest([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], "Byte-fill Compression");
+	comptest([1,2,1,2,1,2,1,2,1,2,1,2,1,2], "Word-fill Compression");
+	comptest([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], "Increasing Byte-fill Compression");
+	comptest([1,3,3,3,3,3,7,1,3,3,3,3,3,7,1,3,3,3,3,3,7], "Buffer Compression", 13);
+	comptest([1,3,3,3,3,3,7,7,3,3,3,3,3,1,7,3,3,3,3,3,1], "Reverse Buffer Compression", 13);
+	comptest(extended, "Extended byte-fill", 4, true);
+
+	writefln("Tests completed: %d/%d", passes, tests);
 }
