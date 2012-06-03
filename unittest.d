faf97e8c0f76515dab8dc12fe006e198d5f43a62
Cameron Ross <elpenguino@gmail.com>
2012-06-03 02:11:22 -0300

    Improved unit testing: round 2
    Add unit test for bit-reversed buffer copies
    only test() can deal with exceptions properly, so remove that parameter
    from other functions

--- unittest.d
+++ unittest.d
@@ -20,32 +20,31 @@ unittest {
 				output ~= "FAILED";
 		} catch (Throwable e) {
 			output ~= (expectException ? "PASSED" : "FAILED: [" ~ e.msg ~ " on line " ~ text(e.line) ~ " of module " ~ e.file ~ "]");
-			if (expectException)
-				success = true;
+			success = expectException;
 		}
 		writeln(output ~ " " ~ label);
 		if (success)
 			passes++;
 		return success;
 	}
-	void testeqar(T, V)(T[] input, V[] output, string msg, bool expectException = false) {
-		if (!test(input == output, msg, expectException))
+	void testeqar(T, V)(T[] input, V[] output, string msg) {
+		if (!test(input == output, msg))
 			writeln(input);
 	}
-	void decomptest(ubyte[] input, ubyte[] output, string msg, bool expectException = false) {
+	void decomptest(ubyte[] input, ubyte[] output, string msg) {
 		int finalSize;
 		ubyte[] data = decomp.decomp(input, finalSize);
-		if (!test(data == output, "Decomp: " ~ msg, expectException))
+		if (!test(data == output, "Decomp: " ~ msg))
 			writeln(data);
-		if (!test(finalSize == input.length, "Decomp: " ~ msg ~ " size", expectException))
+		if (!test(finalSize == input.length, "Decomp: " ~ msg ~ " size"))
 			writeln(data);
 	}
-	void comptest(ubyte[] input, string msg, int idealsize = -1, bool expectException = false) {
+	void comptest(ubyte[] input, string msg, int idealsize = -1) {
 		auto data = comp.comp(input);
-		if (!test(decomp.decomp(data) == input, "Comp: " ~ msg, expectException))
+		if (!test(decomp.decomp(data) == input, "Comp: " ~ msg))
 			writeln(data);
 		if (idealsize >= 0)
-			if (!test(data.length == idealsize, "Comp: " ~ msg ~ " ideal size", expectException))
+			if (!test(data.length == idealsize, "Comp: " ~ msg ~ " ideal size"))
 				writeln(data);
 	}
 
@@ -58,10 +57,9 @@ unittest {
 	testeqar((cast(ubyte[])[1, 2, 4, 5]).reversebits, cast(ubyte[])[128, 64, 32, 160], "Bit reversal: array");
 	testeqar("HELLO".reversebits, cast(string)[18, 162, 50, 50, 242], "Bit reversal: string");
 	testeqar([].reversebits, [], "Bit reversal: void");
-
 	testeqar([cast(uint)0x80000000].reversebits, [1], "Bit reversal: int");
-	ubyte[] testArray = [0x63, 0x00, 0xFF];
-	testeqar(decomp.decomp(testArray), decomp.decomp(new MemoryStream(testArray)), "Decomp: Stream == array");
+
+	testeqar(decomp.decomp([0x63, 0x00, 0xFF]), decomp.decomp(new MemoryStream(cast(ubyte[])[0x63, 0x00, 0xFF])), "Decomp: Stream == array");
 
 	test(decomp.decomp([0x80, 0xFF, 0x00, 0xFF]) == [], "Decomp: Uninitialized buffer", true);
 
@@ -72,16 +70,17 @@ unittest {
 	decomptest([0x61, 1, 0x80, 0, 0, 0xFF], [1, 2, 1], "Buffer copy");
 	decomptest([0x61, 1, 0xA0, 0, 0, 0xFF], [1, 2, 128], "Bit-reversed Buffer copy");
 	decomptest([0x61, 1, 0xC1, 0, 1, 0xFF], [1, 2, 2, 1], "Byte-reversed buffer copy");
-	ubyte[] extended = new ubyte[513];
-	extended[] = 1;
-	decomptest([0xE6, 0, 1, 0xFF], extended, "Extended byte fill");
+	ubyte[] testArray = new ubyte[513];
+	testArray[] = 1;
+	decomptest([0xE6, 0, 1, 0xFF], testArray, "Extended byte fill");
 
 	comptest([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], "Byte-fill Compression");
 	comptest([1,2,1,2,1,2,1,2,1,2,1,2,1,2], "Word-fill Compression");
 	comptest([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], "Increasing Byte-fill Compression");
 	comptest([1,3,3,3,3,3,7,1,3,3,3,3,3,7,1,3,3,3,3,3,7], "Buffer Compression", 13);
 	comptest([1,3,3,3,3,3,7,7,3,3,3,3,3,1,7,3,3,3,3,3,1], "Reverse Buffer Compression", 13);
-	comptest(extended, "Extended byte-fill", 4, true);
+	comptest([1,3,3,3,3,3,7,128,192,192,192,192,192,224,128,192,192,192,192,192,224], "Bit-reversed Buffer Compression", 13);
+	comptest(testArray, "Extended byte-fill", 4);
 
 	writefln("Tests completed: %d/%d", passes, tests);
 }
